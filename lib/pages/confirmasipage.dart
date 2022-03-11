3aebda919912578b55a6db9ce39bf16d576f9f2e
prim <kuprim88@gmail.com>
2022-03-11 15:17:49 +0700

    mbo

--- lib/pages/confirmasipage.dart
+++ lib/pages/confirmasipage.dart
@@ -23,7 +23,7 @@ class _ConfirmasiPageState extends State<ConfirmasiPage> {
           alignment: Alignment.center,
           children: <Widget>[
             Container(
-              color: Color(kSecondColor),
+              color: Color(kFirstColor),
             ),
             Padding(
               padding: EdgeInsets.only(
@@ -49,53 +49,271 @@ class _ConfirmasiPageState extends State<ConfirmasiPage> {
                     )),
               ),
             ),
-            Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: size.height * 0.2),
-                  child: Container(
-                      alignment: Alignment(0, 0),
-                      width: size.width * 0.8,
-                      height: size.height * 0.2,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(10),
-                            topRight: Radius.circular(10)),
-                        color: Colors.white,
-                      ),
-                      child: Text(
-                        "hgfdsa",
-                        style: TextStyle(
-                            fontFamily: 'Poppins',
-                            color: Colors.black,
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold),
-                      )),
-                ),
-                Container(
-                    alignment: Alignment(0, 0),
-                    width: size.width * 0.8,
-                    height: size.height * 0.2,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.only(
-                          bottomRight: Radius.circular(10),
-                          bottomLeft: Radius.circular(10)),
-                    ),
-                    child: Text(
-                      "hgfdsa",
-                      style: TextStyle(
-                          fontFamily: 'Poppins',
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold),
-                    )),
-              ],
+            
+            Padding(
+              padding: EdgeInsets.only(bottom: size.height * 0.4),
+              child: Container(
+                  alignment: Alignment(0, -0.5),
+                  width: size.width * 0.8,
+                  height: size.height * 0.2,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                    color: Color(kFourthColor),
+                  ),
+                  child: Text(
+                    "KONFIRMASI ABSENSI",
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Color(kThirdColor),
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold),
+                  )),
             ),
-
-            // Padding(padding:
-            // EdgeInsets.only()
-            // )
+            Padding(
+              padding: EdgeInsets.only(top: size.height * 0.25),
+              child: Container(
+                width: size.width * 0.8,
+                height: size.height * 0.6,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: size.height * 0.01),
+                      child: Container(
+                        height: size.height * 0.04,
+                        width: size.width * 0.75,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: size.width * 0.075,
+                              top: size.height * 0.01),
+                          child: Text(
+                            "NAMA",
+                            style: TextStyle(
+                                fontFamily: 'Poppins',
+                                color: Color(kThirdColor),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: size.height * 0.005),
+                      child: Container(
+                        color: Color(kFourthColor),
+                        height: size.height * 0.03,
+                        width: size.width * 0.6,
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 3, left: 10),
+                          child: Text(
+                            "Andika Cakra Langit",
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: size.height * 0.01),
+                      child: Container(
+                        height: size.height * 0.04,
+                        width: size.width * 0.75,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: size.width * 0.075,
+                              top: size.height * 0.01),
+                          child: Text(
+                            "KELAS",
+                            style: TextStyle(
+                                fontFamily: 'Poppins',
+                                color: Color(kThirdColor),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: size.height * 0.005),
+                      child: Container(
+                        color: Color(kFourthColor),
+                        height: size.height * 0.03,
+                        width: size.width * 0.6,
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 3, left: 10),
+                          child: Text(
+                            "XII RPL A",
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: size.height * 0.01),
+                      child: Container(
+                        height: size.height * 0.04,
+                        width: size.width * 0.75,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: size.width * 0.075,
+                              top: size.height * 0.01),
+                          child: Text(
+                            "MATA PELAJARAN",
+                            style: TextStyle(
+                                fontFamily: 'Poppins',
+                                color: Color(kThirdColor),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: size.height * 0.005),
+                      child: Container(
+                        color: Color(kFourthColor),
+                        height: size.height * 0.03,
+                        width: size.width * 0.6,
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 3, left: 10),
+                          child: Text(
+                            "BAHASA KEJAWEN",
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: size.height * 0.01),
+                      child: Container(
+                        height: size.height * 0.04,
+                        width: size.width * 0.75,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: size.width * 0.075,
+                              top: size.height * 0.01),
+                          child: Text(
+                            "GURU PENGAJAR",
+                            style: TextStyle(
+                                fontFamily: 'Poppins',
+                                color: Color(kThirdColor),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: size.height * 0.005),
+                      child: Container(
+                        color: Color(kFourthColor),
+                        height: size.height * 0.03,
+                        width: size.width * 0.6,
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 3, left: 10),
+                          child: Text(
+                            "Dewi Rahayu S.pd",
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: size.height * 0.01),
+                      child: Container(
+                        height: size.height * 0.04,
+                        width: size.width * 0.75,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: size.width * 0.075,
+                              top: size.height * 0.01),
+                          child: Text(
+                            "JADWAL PENGARAN & DESKRIPSI",
+                            style: TextStyle(
+                                fontFamily: 'Poppins',
+                                color: Color(kThirdColor),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: size.height * 0.005),
+                      child: Container(
+                        color: Color(kFourthColor),
+                        height: size.height * 0.03,
+                        width: size.width * 0.6,
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 3, left: 10),
+                          child: Text(
+                            "Andika Cakra Langit",
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: size.height * 0.03),
+                      child: Container(
+                        height: size.height * 0.06,
+                        width: size.width * 0.6,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Color(kThirdColor)),
+                        child: TextButton(
+                          onPressed: () {
+                            Get.off(NavbarQr());
+                          },
+                          child: Text(
+                            "Kirim",
+                            style: TextStyle(fontSize: 24, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                left: size.width *0.43,
+                bottom: size.height * 0.7),
+              child: Container(
+                width: size.width * 0.3,
+                height: size.height * 0.15,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/done 1.png"),
+                      fit: BoxFit.fill),
+                ),
+              ),
+            ),
           ],
         ),
       ),
